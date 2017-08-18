5221ac9afb54e8a45bcf1ce0ec8a6acaff9847af
dk\ivd <ivd@sitecore.net>
2017-08-18 17:06:04 +0800

    Remove unused references in UnityApplication

--- src/Sitecore.Support.166359/UnityApplication.cs
+++ src/Sitecore.Support.166359/UnityApplication.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 using Microsoft.Practices.Unity;
 
 namespace Sitecore.Support.ContentSearch.SolrProvider.UnityIntegration
